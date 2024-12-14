8f740c3a3929645368bbf3eb2da7a2e210eebb4d
anas-dev0 <anas.aouini@supcom.tn>
2024-12-14 15:03:42 +0100

    aa

--- script.js
+++ script.js
@@ -1,8 +1,23 @@
-const plus=document.querySelector(".bplus"),output=document.querySelector(".in"),reset=document.querySelector(".reset"),one=document.querySelector(".one"),two=document.querySelector(".two")
+const plus=document.querySelector(".plus"),output=document.querySelector(".in"),reset=document.querySelector(".reset"),one=document.querySelector(".one"),two=document.querySelector(".two")
+const three=document.querySelector(".three")
+const point=document.querySelector(".point")
+const zero=document.querySelector(".zero")
+const equal=document.querySelector(".equal")
+const div=document.querySelector(".div")
+const mul=document.querySelector(".mul")
+
 plus.addEventListener("click",(e)=>{
     output.textContent +="+"
 })
 reset.addEventListener("click",(e)=>{
     output.textContent=""
 })
-
+one.addEventListener("click",(e)=>{
+    output.textContent+="1"
+})
+two.addEventListener("click",(e)=>{
+    output.textContent+="2"
+})
+equal.addEventListener("click",(e)=>{
+    
+})
