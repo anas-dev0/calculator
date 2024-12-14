fa66b9e8338595034c69ed10f81c947f0e0e3d8a
salah637 <mohamedsalah.zrigua@supcom.tn>
2024-12-14 15:19:22 +0100

    jhbuyv

--- script.js
+++ script.js
@@ -1,26 +1,50 @@
-const plus=document.querySelector(".bplus"),output=document.querySelector(".in"),reset=document.querySelector(".reset"),one=document.querySelector(".one"),two=document.querySelector(".two")
+
+
+const plus=document.querySelector(".plus"),output=document.querySelector(".in"),reset=document.querySelector(".reset"),one=document.querySelector(".one"),two=document.querySelector(".two")
+const three=document.querySelector(".three")
+const point=document.querySelector(".point")
+const zero=document.querySelector(".zero")
+const equal=document.querySelector(".equal")
+const div=document.querySelector(".div")
+const mul=document.querySelector(".mul")
+const nine=document.querySelector(".nine")
+const eight=document.querySelector(".eight")
+const seven=document.querySelector(".seven")
+const six=document.querySelector(".six")
+
+
+
+one.addEventListener("click",(e)=>{
+    output.textContent+="1"
+})
+two.addEventListener("click",(e)=>{
+    output.textContent+="2"
+})
+equal.addEventListener("click",(e)=>{
+    output.textContent+="="
+})
 plus.addEventListener("click",(e)=>{
     output.textContent +="+"
 })
 reset.addEventListener("click",(e)=>{
     output.textContent=""
 })
-const nine=document.querySelector(".nine")
+
 nine.addEventListener("click",(e)=>{
     output.textContent +="9"
 })
-const eight=document.querySelector(".eight")
+
 eight.addEventListener("click",(e)=>{
     output.textContent +="8"
 })
-const seven=document.querySelector(".seven")
+
 seven.addEventListener("click",(e)=>{
     output.textContent +="7"
 })
-const six=document.querySelector(".six")
+
 six.addEventListener("click",(e)=>{
     output.textContent +="6"
 })
-console.log("six")
 
 
+console.log(3*4)
