05d837c76d8f4f1488e8ccaa98e4b38e052602d3
salah637 <mohamedsalah.zrigua@supcom.tn>
2024-12-14 15:04:07 +0100

    man

--- script.js
+++ script.js
@@ -5,4 +5,22 @@ plus.addEventListener("click",(e)=>{
 reset.addEventListener("click",(e)=>{
     output.textContent=""
 })
+const nine=document.querySelector(".nine")
+nine.addEventListener("click",(e)=>{
+    output.textContent +="9"
+})
+const eight=document.querySelector(".eight")
+eight.addEventListener("click",(e)=>{
+    output.textContent +="8"
+})
+const seven=document.querySelector(".seven")
+seven.addEventListener("click",(e)=>{
+    output.textContent +="7"
+})
+const six=document.querySelector(".six")
+six.addEventListener("click",(e)=>{
+    output.textContent +="6"
+})
+console.log("six")
+
 
